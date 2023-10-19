1ac2313df37eb4a57e0315399a85469f92810af7
sere06001 <sere06001@utb.vaxjo.se>
2023-10-19 15:26:05 +0200

    Add project files.

---
+++ Uppgift 4.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if (siffra == 9)
+                {
+                    siffra = -1;
+                }
+                Console.Write($"{siffra + 1}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
